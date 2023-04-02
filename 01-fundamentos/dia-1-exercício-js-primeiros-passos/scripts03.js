c0cbabeaffdf5accb1c20c671f6bd1314653d437
Lisley de Oliveira Francisco <lisleyolifra.bh@gmail.com>
2023-04-02 00:06:13 -0300

    Realizando requisito 4

---
+++ 01-fundamentos/dia-1-exercício-js-primeiros-passos/scripts03.js
@@ -0,0 +1,12 @@
+let num = 13;
+
+if (num > 0) {
+    console.log("positive");
+} else if (num < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
+
+// 4 - Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne: “positive”, se esse valor for positivo; 
+// “negative”, se esse valor for negativo, e “zero”, caso esse valor não seja nem positivo nem negativo.
