9952fa6e2847d9c2efe7d2b12e4a20356c738a2e
Lisley de Oliveira Francisco <lisleyolifra.bh@gmail.com>
2023-04-02 00:32:11 -0300

    Atualizando exercício - requisito 5

---
+++ 01-fundamentos/dia-1-exercício-js-primeiros-passos/scripts04.js
@@ -0,0 +1,20 @@
+let l1 = 13; 
+let l2 = 69;
+let l3 = 31;
+let soma = l1 + l2 + l3;
+
+if (soma <= 180) {
+    console.log('true')
+} else {
+    console.log('false')
+} 
+    if (l1 >= 0 && l2 >= 0 && l3 >= 0) {
+        console.log('Válido')
+    } else {
+        console.log('Error message!')
+    }
+
+    
+// 5 - 🚀 Utilize if/else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo.
+// Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário.
+// Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
