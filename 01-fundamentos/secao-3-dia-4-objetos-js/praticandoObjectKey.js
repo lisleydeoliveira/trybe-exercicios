6bbd5aa2b668bc71a25c3212d641b12795b17307
Lisley de Oliveira Francisco <lisleyolifra.bh@gmail.com>
2023-04-05 16:15:06 -0300

    Atualizando práticas de aulas

---
+++ 01-fundamentos/secao-3-dia-4-objetos-js/praticandoObjectKey.js
@@ -0,0 +1,43 @@
+Exemplo 1
+
+let coolestTvShow = {
+    name: 'BoJack Horseman',
+    genre: 'adult animation',
+    createdBy: 'Raphael Bob-Waksberg',
+    favoriteCharacter: 'Princess Carolyn',
+    quote: 'Princess Carolyn always lands on her feet.',
+    seasons: 6,
+  };
+
+  console.log(Object.keys(coolestTvShow));
+
+_________________________________________________________________________
+Exemplo 2
+
+let student1 = {
+    html: 'Muito Bom',
+    css: 'Bom',
+    javascript: 'Ótimo',
+    softskills: 'Ótimo',
+  };
+  
+  let student2 = {
+    html: 'Bom',
+    css: 'Ótimo',
+    javascript: 'Ruim',
+    softskills: 'Ótimo',
+    git: 'Bom', // chave adicionada
+  };
+
+  function listSkills(student) {
+        let arrayOfSkills = Object.keys(student);
+            for (let index in arrayOfSkills) {
+                console.log(arrayOfSkills[index] + ', Nível: ' + student[arrayOfSkills[index]]);
+            }
+  };
+
+  console.log('Estudante 1');
+  listSkills(student1);
+
+  console.log('Estudante 2');
+  listSkills(student2);
